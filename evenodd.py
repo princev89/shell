c4f02eedf6b5cc0e1886987ff8cc3bd15256a3e7
princev89 <43395338+princev89@users.noreply.github.com>
2020-03-11 15:12:09 +0530

    Delete evenodd.py

--- evenodd.py
+++
@@ -1,7 +0,0 @@
-x = int(input("please provide any input or 0 to terminate\n"))
-while x!= 0:
-    if(x%2 == 0):
-        print(str(x) + " is an even number")
-    else:
-        print(str(x) + " is not an even it is the odd number")
-    x = int(input("please provide any input or 0 to terminate\n"))
