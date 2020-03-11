f26200c0352819ea570569f9cd7eae6908b4925f
princev89 <43395338+princev89@users.noreply.github.com>
2020-03-11 15:17:35 +0530

    Create minimum.py

---
+++ minimum.py
@@ -0,0 +1,7 @@
+x=input("Please input the value of the first number\n")
+y=input("Please input the value of the second number\n")
+
+if (int(x) < int(y)):
+    print("The minimum number is the first number and its value is: " + x)
+else:
+    print("The minimum number is the second number and its value is: " + y)
