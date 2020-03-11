75f3e3fa24f7ae8345be7ffb1fbfe1ffd6d243d5
princev89 <43395338+princev89@users.noreply.github.com>
2020-03-11 14:49:21 +0530

    Update set.py

--- set.py
+++ set.py
@@ -1,9 +1,9 @@
 set1 = {0, 2, 4, 6, 8}
 set2 = {1, 2, 3, 4, 5}
 
-count = 0
+set3 = set()
 
 for i in set2:
-    if i in set1:
-        count += 1
-print(count)
+    if not(i in set1):
+        set3.add(i)
+print(set3)
